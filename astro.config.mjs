312fb2d7c81df62762bbc5941c13600996cb7e37
Tom Tanaka <tanaka1201t@gmail.com>
2025-01-28 19:58:01 +0900

    Refactoring the design

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,18 @@ import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://tomtan.dev',
-	integrations: [mdx(), sitemap()],
-	markdown: {
-		rehypePlugins: [ rehypeHeadingIds ],
-		shikiConfig: {
-			theme: 'github-light'
-		}
-	}
+    site: 'https://tomtan.dev',
+    integrations: [
+        mdx(), 
+        sitemap(),
+        tailwind({
+            applyBaseStyles: false,
+        })],
+    markdown: {
+        rehypePlugins: [ rehypeHeadingIds ],
+    }
 });
